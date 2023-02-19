7d311129047d64777ef895db6fd67daf34917bef
okechukwumary20 <benedictamary20@gmail.com>
2023-02-19 10:22:01 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Block of character
+ *Return: always 0 success
+ */
+
+int main(void)
+{
+
+int c = 0;
+{
+for (c = 0; c <= 9; ++c)
+putchar('0' + c);
+putchar('\n');
+									}
+return (0);
+}
+
